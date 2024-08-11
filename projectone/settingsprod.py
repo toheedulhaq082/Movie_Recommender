7cecdc714b4f327ec231f2ccf2cd6fb335188e54
Toheed Ul Haq <binazadadv@gmail.com>
2024-08-11 16:29:02 -0700

    added allowed host

--- projectone/settingsprod.py
+++ projectone/settingsprod.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moviechoose.com', 'www.moviechoose.com']
 
 
 # Application definition
