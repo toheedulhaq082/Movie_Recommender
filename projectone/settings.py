c46817f395238840d1c0301001741df0e6c2f98f
Toheed Ul Haq <binazadadv@gmail.com>
2024-08-12 11:23:19 -0700

    changed staticfiles

--- projectone/settings.py
+++ projectone/settings.py
@@ -127,10 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/css/'),
-    os.path.join(BASE_DIR, 'static/css/dist/'),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'theme', 'static'),
 ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
